ceb9561f016764dfb524cea325a0a57931d97377
Rida Defitria <76247842+defitria@users.noreply.github.com>
2022-02-01 00:43:40 +0700

    adding new  table
    adding new  table

--- database/bappeda.sql
+++ database/bappeda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2021 at 08:49 PM
+-- Generation Time: Jan 31, 2022 at 06:41 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.16
 
@@ -88,7 +88,7 @@ CREATE TABLE `arsip` (
 INSERT INTO `arsip` (`id`, `judul`, `nama`, `kategori`, `terbit`, `tanggal`) VALUES
 (3, 'Contoh Arsip 1', '34-Article Text-119-2-10-20181114.pdf', 'arsip IPW', 'N', '2021-10-18 12:04:19'),
 (6, 'Contoh Arsip 2', 'jurnal_13173.pdf', 'arsip IPW', 'Y', '2021-10-18 16:08:55'),
-(7, 'Paparan Jalan Baru Bengkulu-Sumsel 20 Juni 2020', 'Paparan Jalan Baru Bengkulu-Sumsel 20 Juni 2020 ver tanpa video.pdf', 'arsip IPW', 'Y', '2021-10-21 14:46:38');
+(7, 'Paparan Jalan Baru Bengkulu-Sumsel 20 Juni 2020', 'Paparan Jalan Baru Bengkulu-Sumsel 20 Juni 2020 ver tanpa video.pdf', 'arsip IPW', 'Y', '2020-10-21 14:46:38');
 
 -- --------------------------------------------------------
 
@@ -111,7 +111,7 @@ CREATE TABLE `berita` (
 
 INSERT INTO `berita` (`id`, `judul`, `isi`, `gambar`, `terbit`, `tanggal`) VALUES
 (2, 'Tes Judul Berita 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus bibendum turpis, a volutpat nunc scelerisque nec. Quisque nisl elit, gravida vitae leo vitae, pellentesque lacinia neque. Aliquam fermentum ultrices consectetur. Curabitur non felis posuere, consectetur quam a, porta ligula. Vestibulum risus justo, mattis id ullamcorper non, imperdiet eget lorem. Etiam at lorem massa. Quisque cursus turpis ut consectetur tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc fringilla erat posuere tempor iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed ut odio sit amet mauris interdum condimentum. Etiam sed felis vitae libero tristique viverra. Praesent in molestie enim.\r\n <br><br>\r\nSuspendisse et feugiat felis, at fringilla risus. Proin eget consectetur ipsum, et viverra tellus. Integer malesuada placerat iaculis. Etiam facilisis arcu velit, quis maximus neque pulvinar vitae. Proin imperdiet eu ex vel convallis. Ut porta iaculis auctor. Nunc velit sem, placerat nec turpis a, ornare convallis urna. Phasellus sit amet erat elementum, hendrerit ante sed, rutrum est. Phasellus hendrerit sollicitudin nulla, nec dapibus sapien tempus eget. Vivamus a tortor vel neque placerat dapibus. Cras feugiat nunc velit. Praesent bibendum efficitur magna. Donec egestas justo lectus, sit amet tempor enim aliquam in. Nulla varius leo id sodales mattis. Vivamus elementum, tellus sed fermentum porta, lacus tellus semper lorem, commodo consequat ante orci ut massa.', 'IMG_20210331_093410.jpg', 'Y', '2021-10-21 16:26:52'),
-(3, 'Tes Judul Berita 3', 'In tincidunt sapien eu dui porttitor porttitor. Nulla lobortis sapien ut dui iaculis, vitae ultricies nibh pharetra. Duis in turpis metus. Nunc id quam ex. Donec sed eros convallis, placerat erat a, hendrerit nisl. Aliquam finibus orci vel lectus placerat volutpat. Integer eros ex, ultricies nec nibh vel, dictum vulputate felis. Pellentesque tincidunt metus nec ex commodo sagittis. Quisque elit ipsum, maximus id quam vel, molestie scelerisque ex. Donec eget mauris non magna sagittis fermentum non vel nisl. Sed dapibus augue vel libero auctor commodo. Integer vulputate maximus nibh ac efficitur. Duis at elit quam. Donec mattis, dui in posuere gravida, augue nulla mattis ipsum, in rutrum sapien libero eget elit. Vivamus id pharetra quam.\r\n<br><br>\r\nNunc suscipit tortor sed hendrerit varius. Fusce congue nibh orci, vitae tincidunt dolor vestibulum ut. Morbi et nulla faucibus, posuere dui sed, fringilla velit. Sed egestas vitae elit nec rutrum. Integer quis nibh a augue posuere imperdiet id eget libero. Nunc venenatis volutpat dictum. Curabitur imperdiet tincidunt nibh et molestie. Nam in mauris non purus fringilla porta.', 'IMG_20210331_105501.jpg', 'N', '2021-10-19 16:27:35');
+(3, 'Tes Judul Berita 3', 'In tincidunt sapien eu dui porttitor porttitor. Nulla lobortis sapien ut dui iaculis, vitae ultricies nibh pharetra. Duis in turpis metus. Nunc id quam ex. Donec sed eros convallis, placerat erat a, hendrerit nisl. Aliquam finibus orci vel lectus placerat volutpat. Integer eros ex, ultricies nec nibh vel, dictum vulputate felis. Pellentesque tincidunt metus nec ex commodo sagittis. Quisque elit ipsum, maximus id quam vel, molestie scelerisque ex. Donec eget mauris non magna sagittis fermentum non vel nisl. Sed dapibus augue vel libero auctor commodo. Integer vulputate maximus nibh ac efficitur. Duis at elit quam. Donec mattis, dui in posuere gravida, augue nulla mattis ipsum, in rutrum sapien libero eget elit. Vivamus id pharetra quam.\r\n<br><br>\r\nNunc suscipit tortor sed hendrerit varius. Fusce congue nibh orci, vitae tincidunt dolor vestibulum ut. Morbi et nulla faucibus, posuere dui sed, fringilla velit. Sed egestas vitae elit nec rutrum. Integer quis nibh a augue posuere imperdiet id eget libero. Nunc venenatis volutpat dictum. Curabitur imperdiet tincidunt nibh et molestie. Nam in mauris non purus fringilla porta.', 'IMG_20210331_105501.jpg', 'Y', '2022-01-28 16:00:30');
 
 -- --------------------------------------------------------
 
@@ -166,6 +166,29 @@ INSERT INTO `galerivideo` (`id`, `video`, `terbit`, `tanggal`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `jawabanadmin`
+--
+
+CREATE TABLE `jawabanadmin` (
+  `id` int(5) NOT NULL,
+  `email_pengunjung` varchar(65) NOT NULL,
+  `pesan_jawaban` text NOT NULL,
+  `tanggal_jawaban` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jawabanadmin`
+--
+
+INSERT INTO `jawabanadmin` (`id`, `email_pengunjung`, `pesan_jawaban`, `tanggal_jawaban`) VALUES
+(2, 'admin4@pngo.com', 'Ya 12', '2022-01-28 18:11:16'),
+(3, 'admin6@pngo.com', 'Ya test admin6@pngo.com', '2022-01-28 18:25:25'),
+(4, 'admin7@pngo.com', 'Ya Test admin7@pngo.com', '2022-01-28 18:27:46'),
+(5, 'admin8@pngo.com', 'ya tes admin8@pngo.com', '2022-01-28 18:29:40');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pengunjung`
 --
 
@@ -182,19 +205,16 @@ CREATE TABLE `pengunjung` (
 --
 
 INSERT INTO `pengunjung` (`no`, `nama`, `email`, `pesan`, `tanggal`) VALUES
-(7, 'Rida Defitria', 'admin@pngo.com', 'test', '2021-09-18 11:57:43'),
-(8, 'Rida Defitria', 'admin@pngo.com', 'testt', '2021-09-18 11:58:13'),
-(9, 'Rida Defitria', 'admin@pngo.com', 'test', '2021-09-18 12:11:16'),
-(10, 'Rida Defitria', 'admin@pngo.com', 'test', '2021-09-18 12:11:55'),
-(11, 'Rida Defitria', 'admin@pngo.com', 'test', '2021-09-18 12:13:11'),
-(12, 'Rida Defitria', 'admin@pngo.com', '123', '2021-09-18 12:14:41'),
-(13, 'Rida Defitria', 'admin@pngo.com', '123', '2021-09-18 12:15:13'),
-(14, 'Rida Defitria', 'admin@pngo.com', '123', '2021-09-18 12:15:22'),
-(15, 'Rida Defitria', 'admin@pngo.com', '123', '2021-09-18 12:15:31'),
-(16, 'Rida Defitria', 'admin@pngo.com', '123', '2021-09-18 12:15:53'),
-(17, 'Rida Defitria', 'admin@pngo.com', '12', '2021-09-18 17:39:17'),
-(18, 'Rida Defitria', 'admin@pngo.com', '12', '2021-09-18 17:39:39'),
-(19, 'Rida Defitria', 'admin@pngo.com', 'Test123', '2021-09-18 17:43:21');
+(9, 'Rida Defitria', 'admin6@pngo.com', 'test', '2022-01-28 17:24:19'),
+(10, 'Rida Defitria', 'admin7@pngo.com', 'test', '2022-01-28 17:24:24'),
+(11, 'Rida Defitria', 'admin8@pngo.com', 'test', '2022-01-28 17:24:28'),
+(12, 'Rida Defitria', 'admin9@pngo.com', '123', '2022-01-28 17:24:32'),
+(14, 'Rida Defitria', 'admin11@pngo.com', '123', '2022-01-28 17:24:51'),
+(15, 'Rida Defitria', 'admin1@pngo.com', '123', '2022-01-28 16:32:11'),
+(16, 'Rida Defitria', 'admin2@pngo.com', '123', '2022-01-28 16:35:43'),
+(17, 'Rida Defitria', 'admin3@pngo.com', '12', '2022-01-28 16:32:05'),
+(18, 'Rida Defitria', 'admin4@pngo.com', '12', '2022-01-28 16:32:00'),
+(19, 'Rida Defitria', 'admin5@pngo.com', 'Test123', '2022-01-28 16:31:55');
 
 --
 -- Indexes for dumped tables
@@ -237,6 +257,12 @@ ALTER TABLE `galerivideo`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `jawabanadmin`
+--
+ALTER TABLE `jawabanadmin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pengunjung`
 --
 ALTER TABLE `pengunjung`
@@ -256,7 +282,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `agenda`
 --
 ALTER TABLE `agenda`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `arsip`
@@ -281,6 +307,12 @@ ALTER TABLE `galerifoto`
 --
 ALTER TABLE `galerivideo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `jawabanadmin`
+--
+ALTER TABLE `jawabanadmin`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `pengunjung`
